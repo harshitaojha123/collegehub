a4105c05054bfdbabc8c7073395696c9cd8ea089
harshitaojha123 <ojhaharshita015@gmail.com>
2026-06-11 13:37:50 +0530

    Remove env files

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "https://collegehub-rose.vercel.app"
+   "https://collegehub-sko3.vercel.app/"
   ],
   credentials: true
 }));
@@ -29,8 +29,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/compare", compareRoutes);
 app.use("/api/profile", profileRoutes);
 
-const PORT = 5000;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
